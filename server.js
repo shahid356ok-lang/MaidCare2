5e8835dc2b5820c4a5fda9504427c84fd9d41c49
shahid356ok-lang <shahid356ok@gmail.com>
2026-09-13 15:36:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+function login() {
+    alert("Login feature coming soon!");
+}
+
+function scrollToHelpers() {
+    document.getElementById("helpers").scrollIntoView();
+}
+
+function bookHelper(name) {
+    alert("Booking request sent for " + name + "!");
+}
+
+function selectPlan(plan) {
+    alert(plan + " plan selected!");
+}
